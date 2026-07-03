eaed95fbfebf7e67fc5525aeda6cd2a6657c7b56
zingzy <admin@spoo.me>
2026-07-03 21:40:28 +0530

    feat: stats and hotness consumer classes with pluggable hot-url actions

---
+++ services/click/consumers/__init__.py
@@ -0,0 +1,15 @@
+from services.click.consumers.hotness import (
+    HotUrl,
+    HotUrlAction,
+    HotUrlDetector,
+    LogHotUrlAction,
+)
+from services.click.consumers.stats import StatsClickConsumer
+
+__all__ = [
+    "HotUrl",
+    "HotUrlAction",
+    "HotUrlDetector",
+    "LogHotUrlAction",
+    "StatsClickConsumer",
+]
